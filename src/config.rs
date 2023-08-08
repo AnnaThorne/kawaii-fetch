7a88812f5a69047c8bc0ca53bd01a8d4938a3a32
effie <havealittlefaith2096@gmail.com>
2023-08-08 18:54:41 +0100

    sussy config stuff

---
+++ src/config.rs
@@ -0,0 +1,101 @@
+use directories::BaseDirs;
+use serde::{Deserialize, Serialize};
+use std::{fs::read_to_string, io::Error as IOError, path::PathBuf};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ConfigToml {
+    main: Option<ConfigTomlMain>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ConfigTomlMain {
+    widgets: Option<String>,
+}
+
+#[derive(Debug)]
+pub struct Config {
+    pub widgets: Vec<String>,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            widgets: vec![
+                "nameu".to_string(),
+                "os_vewsiown".to_string(),
+                "howst_name".to_string(),
+                "kewnel_vewsion".to_string(),
+                "cpuwu".to_string(),
+                "totaw_memowy".to_string(),
+                "used_memowy".to_string(),
+            ],
+        }
+    }
+}
+
+impl Config {
+    pub fn new() -> Self {
+        let mut user_config_filepath: String = "./kawaii.toml".to_string();
+        if let Some(base_dirs) = BaseDirs::new() {
+            let mut kawaii_config_path = PathBuf::from(base_dirs.config_dir());
+            kawaii_config_path.push("kawaiifetch/kawaii.toml");
+
+            if kawaii_config_path.exists() {
+                match kawaii_config_path.into_os_string().into_string() {
+                    #![allow(clippy::single_match)]
+                    Ok(config_path) => {
+                        user_config_filepath = config_path;
+                    }
+                    Err(e) => {
+                        println!("{:?}", e);
+                    }
+                }
+            }
+        };
+
+        // prioritize user config
+        let config_filepaths: [&str; 2] = [user_config_filepath.as_str(), "./default_config.toml"];
+
+        let mut content: String = "".to_string();
+
+        for filepath in config_filepaths {
+            let config_path = PathBuf::from(filepath);
+            let config_file_contents: Result<String, IOError> = read_to_string(config_path);
+
+            match config_file_contents {
+                Ok(result) => {
+                    content = result;
+                    break;
+                }
+                Err(e) => {
+                    println!("Error: {}; {}", e, filepath);
+                }
+            }
+        }
+
+        let config_toml: ConfigToml = toml::from_str(&content).unwrap_or_else(|_| {
+            println!("Failed to parse config file.");
+            ConfigToml { main: None }
+        });
+
+        let widgets: Vec<String> = match config_toml.main {
+            Some(config_main) => {
+                let main_widgets: String = config_main.widgets.unwrap_or_else(|| {
+                    println!("No widgets field specified in main.\nUsing defaults");
+                    Config::default().widgets.join(" ")
+                });
+
+                main_widgets
+                    .split_whitespace()
+                    .map(|w| w.to_string())
+                    .collect()
+            }
+            None => {
+                println!("Missing table main.");
+                Config::default().widgets
+            }
+        };
+
+        Config { widgets }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,68 @@
 use std::collections::HashMap;
 use sysinfo::{System, SystemExt};
 
+mod config;
+use config::Config;
+
 fn main() {
+    let config: Config = Config::new();
+
     print_kawaii_ascii();
-    print_kawaii_info();
+    print_kawaii_info(config.widgets);
 }
 
-fn get_system_info () -> HashMap<String, String> {
+fn get_system_info() -> HashMap<String, String> {
     // init structs
     let mut sus = System::new_all();
     sus.refresh_all();
     let mut kawaii_info: HashMap<String, String> = HashMap::new();
 
     // system infowmation
-    kawaii_info.insert("nameu".to_string(), sus.name().unwrap());
-    kawaii_info.insert("os_vewsiown".to_string(), sus.os_version().unwrap());
-    kawaii_info.insert("howst_name".to_string(), sus.host_name().unwrap());
-    kawaii_info.insert("kewnel_vewsion".to_string(), sus.kernel_version().unwrap());
+    kawaii_info.insert("nameu".to_string(), sus.name().unwrap_or_default());
+    kawaii_info.insert(
+        "os_vewsiown".to_string(),
+        sus.os_version().unwrap_or_default(),
+    );
+    kawaii_info.insert(
+        "howst_name".to_string(),
+        sus.host_name().unwrap_or_default(),
+    );
+    kawaii_info.insert(
+        "kewnel_vewsion".to_string(),
+        sus.kernel_version().unwrap_or_default(),
+    );
 
     // cpuwu infowmation
     kawaii_info.insert("cpuwu".to_string(), sus.cpus().len().to_string());
 
     // memowy section
-    kawaii_info.insert("totaw_memowy".to_string(), sus.total_memory().to_string());
-    kawaii_info.insert("used_memowy".to_string(), sus.used_memory().to_string());
+    kawaii_info.insert(
+        "totaw_memowy".to_string(),
+        format!("{:.2} GiB", (sus.total_memory() as f64) * 1e-9).to_string(),
+    );
+    kawaii_info.insert(
+        "used_memowy".to_string(),
+        format!("{:.2} GiB", (sus.used_memory() as f64) * 1e-9).to_string(),
+    );
 
-    return kawaii_info;
+    kawaii_info
 }
 
-fn print_kawaii_info() {
+fn print_kawaii_info(widgets: Vec<String>) {
     let kawaii_info: HashMap<String, String> = get_system_info();
-    // os
-    println!("{}{:#?}", "nameu: ", kawaii_info.get("nameu").unwrap());
-    println!("{}{:#?}", "os_vewsiown: ", kawaii_info.get("os_vewsiown").unwrap());
-    println!("{}{:#?}", "howst_name: ", kawaii_info.get("howst_name").unwrap());
-    println!("{}{:#?}", "kewnel_vewsion: ", kawaii_info.get("kewnel_vewsion").unwrap());
-    // hadwawe
-    println!("{}{:#?}", "cpuwu: ", kawaii_info.get("cpuwu").unwrap());
-    println!("{}{:#?}", "totaw_memowy: ", kawaii_info.get("totaw_memowy").unwrap());
-    println!("{}{:#?}", "used_memowy: ", kawaii_info.get("used_memowy").unwrap());
-    //for (kawaii_key, kawaii_value) in kawaii_info {
-    //    println!("{}{:#?}", kawaii_key, kawaii_value);
-    //}
+
+    for widget in widgets {
+        if kawaii_info.contains_key(&widget) {
+            println!("{}: {}", widget, kawaii_info.get(&widget).unwrap());
+        }
+    }
 }
 
 // print out cute ascii (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
-fn print_kawaii_ascii () {
+fn print_kawaii_ascii() {
     for (_line, kawaii_line) in KAWAII_ASCII.iter().enumerate() {
         println!("{}", kawaii_line);
     }
 }
 
-const KAWAII_ASCII: &[&str] = &[
-  "  (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ "
-];
+const KAWAII_ASCII: &[&str] = &["(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ "];
